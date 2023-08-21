ef822021e32ab78127a9ddbf0f1a094c6a373eb0
TrymVei <tve@variant.no>
2023-08-21 14:23:41 +0200

    feat: component for displaying layout on two columns

---
+++ app/components/layoutWrapper.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useChatStore } from "../store";
+import { isMobileScreen } from "../utils";
+import styles from "./home.module.scss";
+
+export default function LayoutWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const config = useChatStore((state) => state.config);
+
+  return (
+    <div
+      className={`${
+        config.tightBorder && !isMobileScreen()
+          ? styles["tight-container"]
+          : styles.container
+      }`}
+    >
+      {children}
+    </div>
+  );
+}
