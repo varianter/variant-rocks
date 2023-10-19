651204e7f3b9c6e1c00a5abb70519e08f752a19d
olavbm <olavmarkussen@hotmail.no>
2023-10-19 16:37:34 +0200

    add fallback string when employeeCV fail to render

--- app/components/employeeCV.tsx
+++ app/components/employeeCV.tsx
@@ -101,7 +101,9 @@ export default function EmployeeCV({
   employeeAlias,
 }: SummaryOfQualificationProps) {
   return (
-    <ErrorBoundary>
+    <ErrorBoundary
+      fallback={<p> Something went wrong with the EmployeeCV! </p>}
+    >
       <_EmployeeCV employeeAlias={employeeAlias} />
     </ErrorBoundary>
   );
