afdb12603d67e71439fe7a6bec4f2a17c8aaeab5
haakoaho <hakon.hoff@outlook.com>
2023-11-09 10:40:33 +0100

    redirect til login hvis refresh token er utløpt. Den utløper etter 24 timer med inaktivitet.

--- app/api/auth/[...nextauth]/typing.ts
+++ app/api/auth/[...nextauth]/typing.ts
@@ -3,10 +3,12 @@ import { JWT } from "next-auth/jwt";
 
 export interface CustomSession extends Session {
   access_token: string;
+  refresh_token_expiry: number;
 }
 
 export interface CustomToken extends JWT {
   expiry: number; //unix timestamp
+  refresh_expiry: number;
   access_token: string | undefined;
   refresh_token: string | undefined;
 }
