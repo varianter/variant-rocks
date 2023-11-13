66d64b81937e61f2c6a64ffe74ab9841170286f3
haakoaho <hakon.hoff@outlook.com>
2023-11-13 14:19:14 +0100

    Laget en søkbar dropdown for employees

--- app/salesGPT/page.tsx
+++ app/salesGPT/page.tsx
@@ -1,10 +1,10 @@
-import { redirect } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 
 import { getServerSession } from "next-auth/next";
 
 import { authOptions } from "../api/auth/auth-options";
-import { SalesGPT } from "../components/salesGPT";
 import getAllEmployees from "../function/Employees";
+import EmployeeCV from "../components/employeeCV";
 
 export default async function App() {
   const employees = await getAllEmployees();
@@ -13,6 +13,6 @@ export default async function App() {
     return redirect("/api/auth/signin");
   }
   if (employees) {
-    return <SalesGPT employees={employees} />;
+    return <EmployeeCV employees={employees} />;
   }
 }
